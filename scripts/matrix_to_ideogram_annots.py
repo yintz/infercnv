b52a6b7ae8af4681fcb0be09f6cacf5bd27f03c3
Eric Weitz <eweitz@broadinstitute.org>
2018-03-21 17:19:31 -0400

    Add methods to get gene positions, transform expression matrix; begin porting compute method
    Former-commit-id: 3cb2a2c9b2ace0b3ddbb226d5eebbf1fad274178

--- scripts/matrix_to_ideogram_annots.py
+++ scripts/matrix_to_ideogram_annots.py
@@ -14,29 +14,101 @@ __status__ = 'Development'
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 import json
 
+
 class MatrixToIdeogramAnnots:
 
-    def __init__(self, infercnv_output, cluster_meta, output_path):
+    def __init__(self, infercnv_output, cluster_meta, output_path,
+                 genomic_position_file_path):
+        """Class and parameter docs in module summary and argument parser"""
 
         self.infercnv_output = infercnv_output
         self.cluster_meta = cluster_meta
         self.output_path = output_path
+        self.genomic_position_file_path = genomic_position_file_path
+
+        self.genes = self.get_genes()
 
         self.write_annots()
 
     def write_annots(self):
-
-        output_path = self.output_path
+        """Write Ideogram.js annotations JSON file"""
 
         ideogram_annots = self.get_annots()
 
         ideogram_annots_json = json.dumps(ideogram_annots)
 
-        with open(output_path) as f:
+        with open(self.output_path) as f:
             f.write(ideogram_annots_json)
 
     def get_annots(self):
-        return None
+        """Get Ideogram.js annotations"""
+
+        annots = []
+
+
+        return annots
+
+    def get_genes(self):
+        """Convert inferCNV genomic position file into useful 'genes' dict"""
+
+        genes = {}
+
+        with open(self.genomic_position_file_path) as f:
+            lines = f.readlines()
+
+        for line in lines:
+            columns = line.strip().split()
+            id, chr, start, stop = columns
+            genes[id] = {
+                'id': id,
+                'chr': chr,
+                'start': start,
+                'stop': stop
+            }
+
+        return genes
+
+    def get_expression_matrix_dict(self):
+        """Parse inferCNV output, return dict of cell expressions by gene"""
+
+        em_dict = {}
+
+        with open(self.infercnv_output) as f:
+            lines = f.readlines()
+
+        em_dict['cells'] = lines[0].strip().split(',')
+        genes = {}
+
+        for line in lines[1:]:
+            columns = line.strip().split(',')
+            gene = columns[0]
+            expression_by_cell = columns[1:]
+            genes[gene] = expression_by_cell
+
+        em_dict['genes'] = genes
+
+        return em_dict
+
+    def compute_gene_expression_means(self):
+        """Compute mean expression for each gene across all and each cluster"""
+
+        scores_lists = []
+
+        cluster_names = cluster_meta.keys()
+
+        keys = ['name', 'all'] + cluster_names
+        scores_lists.append(keys)
+
+        em_matrix = self.get_expression_matrix_dict()
+
+        gene_expression_lists = em_matrix['genes'].keys()
+
+        for gene in gene_expression_lists:
+            gene_exp_list = gene_expression_lists[gene]
+
+        expression = 0.0
+
+        return scores_lists
 
 
 def get_cluster_meta(names, paths):
