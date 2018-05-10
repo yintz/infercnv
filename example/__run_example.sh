14963567f0e4aefb030f164e008aa9757f0b3c0b
Brian Haas <brianjohnhaas@gmail.com>
2018-05-10 17:40:41 -0400

    restored original example data
    Former-commit-id: eb0eeb4fc6d1bd6e662bcf027011550e87d551e7

--- example/__run_example.sh
+++ example/__run_example.sh
@@ -1,9 +1,10 @@
 # assumes infercnv is installed.
 
 ../scripts/inferCNV.R \
+    --delim " " \
     --ref_groups "1:235,236:280" \
-    --cutoff 4 \
-    --noise_filter 0.3 \
+    --cutoff 1 \
+    --noise_filter 0.25 \
     --output_dir quickstart \
     --vis_bound_threshold " -1,1" \
     --ref example_refs.txt \
