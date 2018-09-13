bcf9cd3f9d193956647528df7181b7b4c078a804
Brian Haas <brianjohnhaas@gmail.com>
2018-09-13 15:02:42 -0400

    added more documentaiton
    Former-commit-id: bc601658383c333e05d3e8ac628786667213ada6
    Former-commit-id: 976cd5ff0cb8806eba62ce5418f15f4fc35dfebc

--- R/inferCNV_mask_non_DE.R
+++ R/inferCNV_mask_non_DE.R
@@ -49,6 +49,20 @@ mask_non_DE_genes_via_Seurat <- function(infercnv_obj,
 }
 
 
+#' @title .mask_DE_genes()
+#' private function that does the actual masking of expression values in the matrix
+#' according to the specified mask value
+#'
+#' @param infercnv_obj
+#'
+#' @param all_DE_results  DE results list with structure:
+#'                           all_DE_results[[ tumor_type ]] = list(tumor=tumor_type, de_genes=c(geneA, geneB, ...))
+#'
+#' @param mask_val float value to assign to genes that are in the mask set (complement of the DE gene set)
+#'
+#' @return infercnv_obj
+#' 
+
 .mask_DE_genes <- function(infercnv_obj, all_DE_results, mask_val) {
 
     
@@ -128,6 +142,36 @@ seurat_from_infercnv_obj <- function(infercnv_obj, normalize_factor) {
 }
 
 
+
+#' @title get_DE_genes_via_Seurat()
+#'
+#' 
+#' Seurat FindMarkers() is used to define differentially expressed genes.
+#' All tumor types are each compared pairwise to the normal cell type groups.
+#' Any genes not found differentially expressed according to the defined parameters in any
+#' of the tumor/normal comparisons are set to the center_val, effectively masking them.
+#'
+#' @param infercnv_obj
+#'
+#' @param thresh.use  from Seurat::FindMarkers : "Limit testing to genes which show, on average, at least X-fold difference (log-scale) between the two groups of cells. Default is 0.25 Increasing thresh.use speeds up the function, but can miss weaker signals."
+#'
+#' @param min.pct   from Seurat::FindMarkers : "only test genes that are detected in a minimum fraction of min.pct cells in either of the two populations. Meant to speed up the function by not testing genes that are very infrequently expressed."
+#'
+#' @param p_val_threshold maximum allowed p-value
+#'
+#' @param test.use from Seurat::FindMarkers : "Denotes which test to use. Seurat currently implements "bimod" (likelihood-ratio test for single cell gene expression, McDavid et al., Bioinformatics, 2013, default), "roc" (standard AUC classifier), "t" (Students t-test), and "tobit" (Tobit-test for differential gene expression, as in Trapnell et al., Nature Biotech, 2014), 'poisson', and 'negbinom'. The latter two options should only be used on UMI datasets, and assume an underlying poisson or negative-binomial distribution"
+#'
+#' @param normalize_factor normalization factor used by Seurat in total sum count scaling. Typically set to 1e4 or 1e5. Default: estimates from the input data.
+#'
+#' @return DE_results_list, structured like so:
+#'
+#'             all_DE_results[[condition_pair]] = list(tumor=tumor_type,
+#'                                                     normal=normal_type,
+#'                                                     markers=markers,
+#'                                                     de_genes=genes)
+#' 
+ 
+
 get_DE_genes_via_Seurat <- function(infercnv_obj,
                                     thresh.use=0.25,
                                     min.pct=0.50,
@@ -178,6 +222,26 @@ get_DE_genes_via_Seurat <- function(infercnv_obj,
 
 
 
+#'
+#' @title mask_non_DE_genes_basic()
+#'
+#' Mask gene expression in infercnv_obj based on expression values that are found
+#' statistically significantly different between tumor/normal based on specified
+#' statistical test.
+#'
+#' Uses the expression data stored in infercnv_obj@expr.data
+#'
+#' @param infercnv_obj
+#'
+#' @param p_val_thresh  p-value threshold for assigning DE statistically significant 
+#'
+#' @param test.use statistical test to use.  (default: "t" for t-test.) **note, other tests still need to be implemented here. basic plumbing provided.
+#'
+#' @param center_val value to assign to those genes that are not found to be statistically DE.
+#'
+#' @return infercnv_obj
+#' 
+
 
 mask_non_DE_genes_basic <- function(infercnv_obj,
                                     p_val_thresh = 0.05,
@@ -197,7 +261,23 @@ mask_non_DE_genes_basic <- function(infercnv_obj,
 }
 
 
-    
+
+#'
+#' @title get_DE_genes_basic
+#'
+#' Retrieves genes identified as significantly DE based on the corresponding statistical test as applied
+#' to the infercnv_obj@expr.data with tumor/normal comparison
+#'
+#' @param p_val_thresh maximum p-value for defining statistical signficance
+#'
+#' @param test.use  statistical test to use. (default: "t" for t-test)
+#' 
+#' @param return             all_DE_results[[condition_pair]] = list(tumor=tumor_type,
+#'                                                                   normal=normal_type,
+#'                                                                   pvals=pvals,
+#'                                                                   de_genes=genes)
+#'
+
 
 get_DE_genes_basic <- function(infercnv_obj,
                                p_val_thresh = 0.05,
