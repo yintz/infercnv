1a5c0ec10b39e3721bfda835fdba9fce456d64f4
Brian Haas <brianjohnhaas@gmail.com>
2018-09-13 13:27:35 -0400

    added basic de test
    Former-commit-id: 47b0300ca97d65672c83a55e183aaacf7c634b8d
    Former-commit-id: 91f95e1d86a0b0b576e28be80bbefec0942c098b

--- R/inferCNV_mask_non_DE.R
+++ R/inferCNV_mask_non_DE.R
@@ -176,3 +176,89 @@ get_DE_genes_via_Seurat <- function(infercnv_obj,
 
 }
 
+
+
+
+mask_non_DE_genes_basic <- function(infercnv_obj,
+                                    p_val_thresh = 0.05,
+                                    test.use="t",
+                                    center_val=mean(infercnv_obj@expr.data) ) {
+    
+
+    all_DE_results = get_DE_genes_basic(infercnv_obj,
+                                        p_val_thresh=p_val_thresh,
+                                        test.use=test.use)
+
+    infercnv_obj <- .mask_DE_genes(infercnv_obj,
+                                   all_DE_results,
+                                   mask_val=center_val)
+
+    return(infercnv_obj)
+}
+
+
+    
+
+get_DE_genes_basic <- function(infercnv_obj,
+                               p_val_thresh = 0.05,
+                               test.use="t" # other options:  (TBD)
+                               ) {
+    
+
+    
+    ## Find DE genes by comparing the mutant types to normal types
+    normal_types = names(infercnv_obj@reference_grouped_cell_indices)
+    tumor_types = names(infercnv_obj@observation_grouped_cell_indices)
+
+    all_DE_results = list()
+
+    statfxns = list()
+    statfxns[[ "t" ]] <- function(x, idx1, idx2) {
+        vals1 = x[idx1]
+        vals2 = x[idx2]
+
+        # useful way of handling tests that may fail:
+        # https://stat.ethz.ch/pipermail/r-help/2008-February/154167.html
+        res = try(t.test(vals1, vals2), silent=TRUE)
+        
+        if (is(res, "try-error")) return(NA) else return(res$p.value)
+        
+    }
+    
+    statfxn = statfxns[[ test.use ]]
+    
+    ## turn on only DE genes in tumors
+    for (tumor_type in tumor_types) {
+
+        tumor_indices = infercnv_obj@observation_grouped_cell_indices[[ tumor_type ]] 
+        
+        for (normal_type in normal_types) {
+            flog.info(sprintf("Finding DE genes between %s and %s", tumor_type, normal_type))
+
+            normal_indices = infercnv_obj@reference_grouped_cell_indices[[ normal_type ]]
+
+
+            pvals = apply(infercnv_obj@expr.data, 1, statfxn, idx1=normal_indices, idx2=tumor_indices)
+            pvals = unlist(pvals)
+            
+            names(pvals) = rownames(infercnv_obj@expr.data)
+
+            genes = names(pvals)[pvals<p_val_thresh]
+            
+            flog.info(sprintf("Found %d genes as DE", length(genes)))
+
+            condition_pair = paste(tumor_type, normal_type, sep=",")
+            
+            all_DE_results[[condition_pair]] = list(tumor=tumor_type,
+                                                    normal=normal_type,
+                                                    pvals=pvals,
+                                                    de_genes=genes)
+            
+            
+        }
+    }
+    
+    return(all_DE_results)
+
+}
+
