2a607e5917b8d0b9c0a7c837e16f88664fd79c17
Brian Haas <brianjohnhaas@gmail.com>
2018-11-18 11:46:20 -0500

    added utility for plotting the hidden spike heatmap
    Former-commit-id: a737288d8070766a1634487ff7aff99bec1de819
    Former-commit-id: 8d8cf9fcbb1ed14e3ed30e2596767f7dd7e80fe4

---
+++ scripts/plot_hspike.R
@@ -0,0 +1,24 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages(library("argparse"))
+    
+parser = ArgumentParser()
+parser$add_argument("--infercnv_obj", help="infercnv_obj file", required=TRUE, nargs=1)
+args = parser$parse_args()
+
+library(infercnv)
+
+infercnv_obj_file = args$infercnv_obj
+
+infercnv_obj = readRDS(infercnv_obj_file)
+
+if (! is.null(infercnv_obj@.hspike)) {
+    out_prefix = paste0(infercnv_obj_file, '.hspike')
+    plot_cnv(infercnv_obj@.hspike,
+             output_filename=basename(out_prefix))
+} else {
+    message("no hspike to plot")
+}
+
+
+
