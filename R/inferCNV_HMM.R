f2b9a45b68e589bea62f19c498b33d7e909b3dbd
Brian Haas <brianjohnhaas@gmail.com>
2018-11-19 12:31:49 -0500

    includes initial HMM implementation
    Former-commit-id: b77f185ed2864e4b9454326aa232604765e6b050
    Former-commit-id: ba61d05611d7a80fa354457c411587cdb1b094fa

--- R/inferCNV_HMM.R
+++ R/inferCNV_HMM.R
@@ -1,6 +1,11 @@
 
 get_spike_dists <- function(hspike_obj) {
 
+    if (is.null(hspike_obj)) {
+        flog.error("get_spike_dists(hspike_obj): Error, hspike obj is null")
+        stop("Error")
+    }
+    
     gene_expr_by_cnv = .get_gene_expr_by_cnv(hspike_obj)
         
     cnv_mean_sd = .get_gene_expr_mean_sd_by_cnv(gene_expr_by_cnv)
@@ -71,3 +76,70 @@ get_spike_dists <- function(hspike_obj) {
     return(p)
 }
 
+
+
+predict_CNV_via_HMM  <- function(infercnv_obj, cnv_mean_sd=get_spike_dists(infercnv_obj@.hspike), t=1e-6) {
+    
+    flog.info("predict_CNV_via_HMM()")
+    
+    ##                      states: 0,   0.5,    1,     1.5,     2,    3
+    state_transitions = matrix( c(1-5*t,  t,     t,      t,      t,    t,
+                                  t,     1-5*t,  t,      t,      t,    t,
+                                  t,      t,     1-5*t,  t,      t,    t,
+                                  t,      t,     t,    1-5*t,    t,    t,
+                                  t,      t,     t,      t,    1-5*t,  t,
+                                  t,      t,     t,      t,      t,  1-5*t),
+                               byrow=T,
+                               nrow=6)
+
+    delta=c(t,      t,     t,    1-5*t,    t,    t) # more likely normal,
+
+    state_emission_params = list(mean=c(cnv_mean_sd[["cnv:0.01"]]$mean,
+                                        cnv_mean_sd[["cnv:0.5"]]$mean,
+                                        cnv_mean_sd[["cnv:1"]]$mean,
+                                        cnv_mean_sd[["cnv:1.5"]]$mean,
+                                        cnv_mean_sd[["cnv:2"]]$mean,
+                                        cnv_mean_sd[["cnv:3"]]$mean),
+
+                                 sd=c(cnv_mean_sd[["cnv:0.01"]]$sd,
+                                      cnv_mean_sd[["cnv:0.5"]]$sd,
+                                      cnv_mean_sd[["cnv:1"]]$sd,
+                                      cnv_mean_sd[["cnv:1.5"]]$sd,
+                                      cnv_mean_sd[["cnv:2"]]$sd,
+                                      cnv_mean_sd[["cnv:3"]]$sd) )
+    
+    chrs = unique(infercnv_obj@gene_order$chr)
+
+
+    expr.data = infercnv_obj@expr.data
+    gene_order = infercnv_obj@gene_order
+    hmm.data = expr.data
+    hmm.data[,] = -1 #init to invalid state
+
+    ## run through each chr separately
+    lapply(chrs, function(chr) {
+        chr_gene_idx = which(gene_order$chr == chr)
+        
+        ## run through each cell for this chromosome:
+        lapply(seq_len(ncol(expr.data)), function(cell_idx) {
+            
+            gene_expr_vals = as.vector(expr.data[chr_gene_idx,cell_idx])
+            
+            hmm <- HiddenMarkov::dthmm(gene_expr_vals,
+                                       state_transitions,
+                                       delta,
+                                       "norm",
+                                       state_emission_params)
+            
+            hmm_trace <- HiddenMarkov::Viterbi(hmm)
+            
+            hmm.data[chr_gene_idx,cell_idx] <<- hmm_trace
+        })
+    })
+    
+    infercnv_obj@expr.data <- hmm.data
+    
+    return(infercnv_obj)
+    
+}
+            
