0457f733f2843b5aecd277769cee84baf671548b
Brian Haas <brianjohnhaas@gmail.com>
2018-11-20 22:15:44 -0500

    prelim incorporation of running hmm on subclusters
    Former-commit-id: 628596e0d58211c6e741fe50a7e845116e528471
    Former-commit-id: 3ae0ce63b93ac421cd67393f2062f4fc4172994d

--- R/inferCNV_HMM.R
+++ R/inferCNV_HMM.R
@@ -1,9 +1,12 @@
 
 get_spike_dists <- function(hspike_obj) {
-
+    
     if (is.null(hspike_obj)) {
         flog.error("get_spike_dists(hspike_obj): Error, hspike obj is null")
         stop("Error")
+    }
+    if (! is.null(hspike_obj@.hspike)) {
+        flog.error("get_spike_dists() should have an hspike obj as param, and this doesn't look like an hspike")
     }
     
     gene_expr_by_cnv = .get_gene_expr_by_cnv(hspike_obj)
@@ -77,11 +80,66 @@ get_spike_dists <- function(hspike_obj) {
 }
 
 
+## Based on number of cells in a clade
+get_hspike_cnv_mean_sd_trend_by_num_cells_fit <- function(hspike_obj, plot=F) {
+    
+    gene_expr_by_cnv <- .get_gene_expr_by_cnv(hspike)
+    cnv_level_to_mean_sd = list()
+    
+    for (cnv_level in names(gene_expr_by_cnv) ) {
+        expr_vals = gene_expr_by_cnv[[ cnv_level ]]
+        nrounds = 100
+        
+        sds = c()
+        for (ncells in seq_len(100)) {
+            means = c()
+            
+            for(i in 1:nrounds) {
+                vals = sample(expr_vals, size=ncells, replace=T)
+                m_val = mean(vals)
+                means = c(means,  m_val)
+            }
+            sds = c(sds, sd(means))
+        }
+        cnv_level_to_mean_sd[[ cnv_level ]] <- sds
+    }
+    
+    if (plot) {
+        df = do.call(rbind, lapply(names(cnv_level_to_mean_sd), function(cnv_level) {
+            sd=cnv_level_to_mean_sd[[ cnv_level ]]
+            data.frame(cnv=cnv_level, num_cells=1:length(sd), sd=sd)
+        }))
 
-predict_CNV_via_HMM  <- function(infercnv_obj, cnv_mean_sd=get_spike_dists(infercnv_obj@.hspike), t=1e-6) {
+        p  = df %>% ggplot(aes(x=log(num_cells),y=log(sd), color=cnv)) + geom_point()
+        pdf("hspike_obs_mean_var_trend.pdf")
+        flog.info("plotting: hspike_obs_mean_var_trend.pdf")
+        plot(p)
+        dev.off()
+    }
+
+    ## fit linear model
+    cnv_level_to_mean_sd_fit = list()
+    for (cnv_level in names(cnv_level_to_mean_sd) ) {
+        sd_vals=cnv_level_to_mean_sd[[ cnv_level ]]
+        num_cells = 1:length(sd_vals)
+
+        flog.info(sprintf("fitting num cells vs. variance for cnv level: %s", cnv_level))
+        fit = lm(log(sd_vals) ~ log(num_cells)) #note, hbadger does something similar, but not for the hmm cnv state levels
+        cnv_level_to_mean_sd_fit[[ cnv_level ]] = fit
+    }
     
-    flog.info("predict_CNV_via_HMM()")
+    return(cnv_level_to_mean_sd_fit)
     
+}
+
+
+
+
+
+
+
+.get_HMM <- function(cnv_mean_sd, t) {
+
     ##                      states: 0,   0.5,    1,     1.5,     2,    3
     state_transitions = matrix( c(1-5*t,  t,     t,      t,      t,    t,
                                   t,     1-5*t,  t,      t,      t,    t,
@@ -107,15 +165,32 @@ predict_CNV_via_HMM  <- function(infercnv_obj, cnv_mean_sd=get_spike_dists(infer
                                       cnv_mean_sd[["cnv:1.5"]]$sd,
                                       cnv_mean_sd[["cnv:2"]]$sd,
                                       cnv_mean_sd[["cnv:3"]]$sd) )
+
+
+
+    HMM_info = list(state_transitions=state_transitions,
+                    delta=delta,
+                    state_emission_params=state_emission_params)
+    
+    return(HMM_info)
+}
+
+    
+
+predict_CNV_via_HMM_on_indiv_cells  <- function(infercnv_obj, cnv_mean_sd=get_spike_dists(infercnv_obj@.hspike), t=1e-6) {
+    
+    flog.info("predict_CNV_via_HMM()")
+    
+    HMM_info  <- .get_HMM(cnv_mean_sd, t)
     
     chrs = unique(infercnv_obj@gene_order$chr)
-
-
+    
     expr.data = infercnv_obj@expr.data
     gene_order = infercnv_obj@gene_order
     hmm.data = expr.data
     hmm.data[,] = -1 #init to invalid state
 
+        
     ## run through each chr separately
     lapply(chrs, function(chr) {
         chr_gene_idx = which(gene_order$chr == chr)
@@ -126,10 +201,10 @@ predict_CNV_via_HMM  <- function(infercnv_obj, cnv_mean_sd=get_spike_dists(infer
             gene_expr_vals = as.vector(expr.data[chr_gene_idx,cell_idx])
             
             hmm <- HiddenMarkov::dthmm(gene_expr_vals,
-                                       state_transitions,
-                                       delta,
+                                       HMM_info[['state_transitions']],
+                                       HMM_info[['delta']],
                                        "norm",
-                                       state_emission_params)
+                                       HMM_info[['state_emission_params']])
             
             hmm_trace <- HiddenMarkov::Viterbi(hmm)
             
@@ -143,3 +218,141 @@ predict_CNV_via_HMM  <- function(infercnv_obj, cnv_mean_sd=get_spike_dists(infer
     
 }
             
+predict_CNV_via_HMM_on_tumor_subclusters  <- function(infercnv_obj,
+                                                      cnv_mean_sd=get_spike_dists(infercnv_obj@.hspike),
+                                                      cnv_level_to_mean_sd_fit=get_hspike_cnv_mean_sd_trend_by_num_cells_fit(infercnv_obj@.hspike),
+                                                      t=1e-6) {
+
+
+    flog.info("predict_CNV_via_HMM()")
+    
+    HMM_info  <- .get_HMM(cnv_mean_sd, t)
+    
+    chrs = unique(infercnv_obj@gene_order$chr)
+    
+    expr.data = infercnv_obj@expr.data
+    gene_order = infercnv_obj@gene_order
+    hmm.data = expr.data
+    hmm.data[,] = -1 #init to invalid state
+
+    tumor_subclusters <- unlist(infercnv_obj@tumor_subclusters[[2]], recursive=F)
+    
+    ## add the normals, so they get predictions too:
+    tumor_subclusters <- c(tumor_subclusters, infercnv_obj@reference_grouped_cell_indices)
+    
+    ## run through each chr separately
+    lapply(chrs, function(chr) {
+        chr_gene_idx = which(gene_order$chr == chr)
+        
+        ## run through each cell for this chromosome:
+        lapply(tumor_subclusters, function(tumor_subcluster_cells_idx) {
+            
+            gene_expr_vals = rowMeans(expr.data[chr_gene_idx,tumor_subcluster_cells_idx])
+            
+            num_cells = length(tumor_subcluster_cells_idx)
+
+            state_emission_params <- .get_state_emission_params(num_cells, cnv_mean_sd, cnv_level_to_mean_sd_fit)
+                        
+            hmm <- HiddenMarkov::dthmm(gene_expr_vals,
+                                       HMM_info[['state_transitions']],
+                                       HMM_info[['delta']],
+                                       "norm",
+                                       state_emission_params)
+            
+            hmm_trace <- HiddenMarkov::Viterbi(hmm)
+            
+            hmm.data[chr_gene_idx,tumor_subcluster_cells_idx] <<- hmm_trace
+        })
+    })
+    
+    infercnv_obj@expr.data <- hmm.data
+    
+    return(infercnv_obj)
+    
+}
+
+
+.get_state_emission_params <- function(num_cells, cnv_mean_sd, cnv_level_to_mean_sd_fit, plot=FALSE) {
+        
+    for (cnv_level in names(cnv_mean_sd)) {
+        fit <- cnv_level_to_mean_sd_fit[[cnv_level]]
+        sd <- exp(predict(fit, newdata=data.frame(num_cells=num_cells))[[1]])
+        cnv_mean_sd[[cnv_level]]$sd <- sd
+    }
+    
+    if (plot) {
+        .plot_cnv_mean_sd_for_num_cells(num_cells, cnv_mean_sd)
+    }
+
+    state_emission_params = list(mean=c(cnv_mean_sd[["cnv:0.01"]]$mean,
+                                        cnv_mean_sd[["cnv:0.5"]]$mean,
+                                        cnv_mean_sd[["cnv:1"]]$mean,
+                                        cnv_mean_sd[["cnv:1.5"]]$mean,
+                                        cnv_mean_sd[["cnv:2"]]$mean,
+                                        cnv_mean_sd[["cnv:3"]]$mean),
+                                 
+                                 sd=c(cnv_mean_sd[["cnv:0.01"]]$sd,
+                                      cnv_mean_sd[["cnv:0.5"]]$sd,
+                                      cnv_mean_sd[["cnv:1"]]$sd,
+                                      cnv_mean_sd[["cnv:1.5"]]$sd,
+                                      cnv_mean_sd[["cnv:2"]]$sd,
+                                      cnv_mean_sd[["cnv:3"]]$sd) )
+    
+    
+    return(state_emission_params)
+}
+
+.plot_cnv_mean_sd_for_num_cells <- function(num_cells, cnv_mean_sd) {
+
+    pdf(sprintf("state_emissions.%d-cells.pdf", num_cells))
+    
+    p = ggplot(data.frame(x=c(0,3)), aes(x)) +
+        stat_function(fun=dnorm,
+                      args=list('mean'=cnv_mean_sd[["cnv:0.01"]]$mean,'sd'=cnv_mean_sd[["cnv:0.01"]]$sd),
+                      aes(colour='cnv:0')) +
+        stat_function(fun=dnorm,
+                      args=list('mean'=cnv_mean_sd[["cnv:0.5"]]$mean,'sd'=cnv_mean_sd[["cnv:0.5"]]$sd),
+                      aes(colour='cnv:0.5')) +
+        stat_function(fun=dnorm,
+                      args=list('mean'=cnv_mean_sd[["cnv:1"]]$mean,'sd'=cnv_mean_sd[["cnv:1"]]$sd),
+                      aes(colour='cnv:1')) +
+        stat_function(fun=dnorm,
+                      args=list('mean'=cnv_mean_sd[["cnv:1.5"]]$mean,'sd'=cnv_mean_sd[["cnv:1.5"]]$sd),
+                      aes(colour='cnv:1.5')) +
+        stat_function(fun=dnorm,
+                      args=list('mean'=cnv_mean_sd[["cnv:2"]]$mean,'sd'=cnv_mean_sd[["cnv:2"]]$sd),
+                      aes(colour='cnv:2')) +
+        stat_function(fun=dnorm,
+                      args=list('mean'=cnv_mean_sd[["cnv:3"]]$mean,'sd'=cnv_mean_sd[["cnv:3"]]$sd),
+                      aes(colour='cnv:3')) +
+        scale_colour_manual("Function",
+                            values=c("blue", "red", "green", "magenta", "orange", "cyan"),
+                            breaks=c("cnv:0","cnv:0.5", "cnv:1", "cnv:1.5", "cnv:2", "cnv:3"))
+    
+    plot(p)
+
+    dev.off()
+
+}
+
+
+.compare_obs_vs_fit_sc_cnv_sd <- function(infercnv_obj,
+                                          cnv_mean_sd=get_spike_dists(infercnv_obj@.hspike),
+                                          cnv_level_to_mean_sd_fit=get_hspike_cnv_mean_sd_trend_by_num_cells_fit(infercnv_obj@.hspike)) {
+    
+    cnv_levels = names(cnv_mean_sd)
+
+    df = do.call(rbind, lapply(cnv_levels, function(cnv_level) {
+        sd_obs=cnv_mean_sd[[cnv_level]]$sd
+        sd_pred=exp(predict(cnv_level_to_mean_sd_fit[[cnv_level]], newdata=data.frame(num_cells=1)))
+        return(data.frame(cnv=cnv_level, sd_obs=sd_obs, sd_pred=sd_pred))
+
+    }))
+    
+    return(df)
+}
+
+
+
+
+    
