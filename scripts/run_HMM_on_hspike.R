2e0118c559b450f19c54403691a9908ffac2a67e
Brian Haas <brianjohnhaas@gmail.com>
2018-11-26 17:41:11 -0500

    util script for running hmm on hspike
    Former-commit-id: fcc9723f0ccdaffda9ede4e91721492bdb1e7eb0
    Former-commit-id: 24a6fc7dfbddd977c4e15ac7d7223bd1e50a7027

---
+++ scripts/run_HMM_on_hspike.R
@@ -0,0 +1,25 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages(library("argparse"))
+    
+parser = ArgumentParser()
+parser$add_argument("--infercnv_obj", help="infercnv_obj file", required=TRUE, nargs=1)
+args = parser$parse_args()
+
+library(infercnv)
+library(ggplot2)
+library(futile.logger)
+
+infercnv_obj_file = args$infercnv_obj
+
+infercnv_obj = readRDS(infercnv_obj_file)
+
+hspike = infercnv_obj@.hspike
+
+hspike = infercnv:::.subcluster_tumors_general(hspike)
+
+hspike.hmm = infercnv:::predict_CNV_via_HMM_on_tumor_subclusters(hspike, infercnv:::get_spike_dists(hspike), infercnv:::get_hspike_cnv_mean_sd_trend_by_num_cells_fit(hspike))
+plot_cnv(hspike.hmm, x.center=3, x.range=c(1,5), output_filename="hspike.hmm")
+
+saveRDS(hspike.hmm, file=sprintf("%s-HMM.obj", infercnv_obj_file))
+
