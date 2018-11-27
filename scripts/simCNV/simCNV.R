c1a75521faf6e50b0b008299d322c8cf1f1bb173
Brian Haas <brianjohnhaas@gmail.com>
2018-11-26 19:54:58 -0500

    sim - support sparse matrix, sampling from normals, and not adding CNVs if not specified
    Former-commit-id: 9ae8d766434e3f831495c99efb8896c2db36ae14
    Former-commit-id: aadfccd94c602ae3546672338a62ed5509f94aa0

--- scripts/simCNV/simCNV.R
+++ scripts/simCNV/simCNV.R
@@ -5,11 +5,13 @@ suppressPackageStartupMessages(library("argparse"))
 parser = ArgumentParser()
 
 parser$add_argument("--normal_counts_matrix", required=T, nargs=1)
-parser$add_argument("--CNV_spec", required=T, nargs=1)
+parser$add_argument("--CNV_spec", required=F, nargs=1, default=NULL)
 parser$add_argument("--gene_order_file", required=T, nargs=1)
 parser$add_argument("--num_normal_cells", type="integer", default=100, nargs=1)
 parser$add_argument("--num_tumor_cells", type="integer", default=100, nargs=1)
-
+parser$add_argument("--readRDS", action="store_true", default=FALSE, help="load matrix obj via readRDS")
+parser$add_argument("--use_real_normals", action="store_true", default=FALSE, help="samples from normals instead of simulating them; note all tumor cells are simulated")
+                   
 
 args = parser$parse_args()
 
@@ -17,10 +19,17 @@ args = parser$parse_args()
 library(edgeR)
 library(infercnv)
 library(dplyr)
-
+library(Matrix)
 
 #' learn distribution parameters:
-data = read.table(args$normal_counts_matrix, header=T, row.names=1)
+data = NULL
+if (args$readRDS) {
+    data = readRDS(args$normal_counts_matrix)
+} else {
+    data = read.table(args$normal_counts_matrix, header=T, row.names=1)
+}
+
+dim(data)
 
 #' normalize first:
 cs = colSums(data)
@@ -28,16 +37,24 @@ median_cs = median(cs)
 data <- sweep(data, STATS=cs, MARGIN=2, FUN="/")
 data <- data * median_cs
 
-# get sim params
+## get sim params
 gene_means = rowMeans(data)
 
 mean_p0_table <- infercnv:::.get_mean_vs_p0_from_matrix(data)
 logistic_params <- infercnv:::.get_logistic_params(mean_p0_table)
 
-#' make simulated normals
-message("Simulating normals")
-normal_sim_matrix <- infercnv:::.get_simulated_cell_matrix(gene_means, mean_p0_table, args$num_normal_cells)
-colnames(normal_sim_matrix) = paste0("normal_", 1:ncol(normal_sim_matrix))
+## make simulated normals
+
+normal_sim_matrix <- NULL
+if (! is.null(args$use_real_normals)) {
+    message("Sampling from normal cells")
+    normal_sim_matrix <- data[, sample(x=1:ncol(data), size=args$num_normal_cells, replace=T)]
+    normal_sim_matrix <- as.matrix(normal_sim_matrix)
+} else {
+    message("Simulating normal cells")
+    normal_sim_matrix <- infercnv:::.get_simulated_cell_matrix(gene_means, mean_p0_table, args$num_normal_cells)
+    colnames(normal_sim_matrix) = paste0("normal_", 1:ncol(normal_sim_matrix))
+}
 
 cell_annots_df = data.frame("cells"=colnames(normal_sim_matrix), "class"="normal")
 
@@ -45,39 +62,41 @@ cell_annots_df = data.frame("cells"=colnames(normal_sim_matrix), "class"="normal
 gene_ordering = read.table(args$gene_order_file, header=F, row.names=NULL, stringsAsFactors=F)
 colnames(gene_ordering) = c('gene', 'chr', 'lend', 'rend')
 
-cnv_info = read.table(args$CNV_spec, header=F, row.names=NULL, stringsAsFactors=F)
-colnames(cnv_info) = c('chr', 'lend', 'rend', 'cnv')
-
-for (i in 1:nrow(cnv_info)) {
-    r = unlist(cnv_info[i,,drop=T])
-    message("simulating cnv for: ", paste(r, collapse="\t"))
-    chrwant = r[1]
-    chrlend = as.integer(r[2])
-    chrrend = as.integer(r[3])
-    cnv = as.double(r[4])
-
-    chr_genes = gene_ordering %>% filter(chr==chrwant)
+if (! is.null(args$CNV_spec)) {
+    cnv_info = read.table(args$CNV_spec, header=F, row.names=NULL, stringsAsFactors=F)
+    colnames(cnv_info) = c('chr', 'lend', 'rend', 'cnv')
     
-    if (chrlend > 0) {
-        # if less than zero, using the whole chr
-        chr_genes = chr_genes %>% filter(lend >= chrlend & rend <= chrrend)
+    for (i in 1:nrow(cnv_info)) {
+        r = unlist(cnv_info[i,,drop=T])
+        message("simulating cnv for: ", paste(r, collapse="\t"))
+        chrwant = r[1]
+        chrlend = as.integer(r[2])
+        chrrend = as.integer(r[3])
+        cnv = as.double(r[4])
+        
+        chr_genes = gene_ordering %>% filter(chr==chrwant)
+        
+        if (chrlend > 0) {
+                                        # if less than zero, using the whole chr
+            chr_genes = chr_genes %>% filter(lend >= chrlend & rend <= chrrend)
+        }
+        
+        idx = which(names(gene_means) %in% chr_genes$gene)
+        if (length(idx)==0) {
+            stop(sprintf("Error, found no genes on chr: %s", chrwant))
+        }
+        
+        gene_means_before = gene_means[idx] 
+        
+        gene_means[idx] = gene_means[idx] * cnv
+        
+        gene_means_after = gene_means[idx]
+        
+        write.table(data.frame(before=gene_means_before, after=gene_means_after), file=sprintf("means.%s", chrwant), quote=F,sep="\t")
     }
-    
-    idx = which(names(gene_means) %in% chr_genes$gene)
-    if (length(idx)==0) {
-        stop(sprintf("Error, found no genes on chr: %s", chrwant))
-    }
-
-    gene_means_before = gene_means[idx] 
-    
-    gene_means[idx] = gene_means[idx] * cnv
-
-    gene_means_after = gene_means[idx]
-
-    write.table(data.frame(before=gene_means_before, after=gene_means_after), file=sprintf("means.%s", chrwant), quote=F,sep="\t")
 }
 
-
+message("Simulating tumor cells")
 tumor_sim_matrix <- infercnv:::.get_simulated_cell_matrix(gene_means, mean_p0_table, args$num_tumor_cells)
 colnames(tumor_sim_matrix) = paste0("tumor_", 1:ncol(tumor_sim_matrix)) 
 
