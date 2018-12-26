a8b4afd80d343aca15b01914b2cdce1ffb083d0e
Brian Haas <brianjohnhaas@gmail.com>
2018-12-26 12:40:04 -0500

    relocated simCNV to separate repo
    Former-commit-id: a4c39d745d054fdce8c54921302dc266676d2974
    Former-commit-id: b45416262380ae65616ca672999998e4668fb60e

--- scripts/simCNV/testdata/run.simCNV.sh
+++
@@ -1,5 +0,0 @@
-../simCNV.R --normal_counts_matrix normal.counts.matrix \
-            --CNV_spec cnvs.want.txt cnvs.want-2.txt cnvs.want-3.txt \
-            --num_tumor_cells 100 60 40 \
-            --gene_order_file gencode_v19_gene_pos.txt \
-            --no_run_infercnv
