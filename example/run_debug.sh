b9416fd054ac7ce3e8fbbd235ac8be65cbe854db
Brian Haas <brianjohnhaas@gmail.com>
2019-02-19 10:01:49 -0500

    simplified example makefile
    Former-commit-id: c749719d12075942446faf257e50ffe904306857
    Former-commit-id: 1277b6386d05aac0d485c6999d448af1a52f58ee

--- example/run_debug.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./run.hmm.R
-../scripts/plot_hspike.R --infercnv_obj output_dir/run.final.infercnv_obj
-../scripts/plot_hspike.by_num_cells.R --infercnv_obj output_dir/run.final.infercnv_obj
-../scripts/run_HMM_on_hspike.R --infercnv_obj output_dir/run.final.infercnv_obj
-
