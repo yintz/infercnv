4776a794ceaf44283dcc2a9c2cc6855ad05ad9c1
Brian Haas <brianjohnhaas@gmail.com>
2019-02-22 19:27:17 -0500

    add
    Former-commit-id: 1180dd8f44f4be1470a8eb0ba12a3885352b8f3b
    Former-commit-id: 52424fa806c9293679e42074c0f141f87c640bf0

---
+++ scripts/examine_normal_sampling_distributions.R
@@ -0,0 +1,147 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages(library("argparse"))
+    
+parser = ArgumentParser()
+parser$add_argument("--infercnv_obj", help="infercnv_obj file", required=TRUE, nargs=1)
+args = parser$parse_args()
+
+library(infercnv)
+library(tidyverse)
+library(futile.logger)
+
+infercnv_obj_file = args$infercnv_obj
+
+infercnv_obj = readRDS(infercnv_obj_file)
+
+if (! infercnv:::has_reference_cells(infercnv_obj)) {
+    stop("Error, cannot tune parameters without reference 'normal' cells defined")
+}
+
+expr_vals <- infercnv_obj@expr.data
+mu = mean(expr_vals)
+sigma = sd(expr_vals)
+nrounds = 1000
+sds = c()
+ngenes = nrow(expr_vals)
+
+normal_samples = infercnv_obj@reference_grouped_cell_indices
+
+num_normal_samples = length(normal_samples)
+
+mean_vals_df = NULL;
+z_p_val = 0.05
+
+num_cells_to_empirical_sd = list()
+
+ncells_partitions = seq (1,100,5)
+for (ncells in ncells_partitions) {
+    means = c()
+    
+    message(sprintf("num cells: %g", ncells))
+
+    cells_counted = 0;
+    
+    for(i in 1:nrounds) {
+        ## pick a random gene
+        rand.gene = sample(1:ngenes)
+        
+        ## pick a random normal cell type
+        rand.sample = sample(num_normal_samples)
+        #rand.sample=1
+        
+        vals = sample(expr_vals[rand.gene, normal_samples[[rand.sample]] ], size=ncells, replace=T)
+        m_val = mean(vals)
+        means = c(means,  m_val)
+
+        cells_counted = cells_counted + length(vals)
+
+        
+    }
+    my.sd = sd(means)
+    sds = c(sds, my.sd)
+
+    num_cells_to_empirical_sd[[ ncells ]] = my.sd
+    
+    df = data.frame(num_cells=ncells, vals=means)
+                                        #print(df)
+    if(is.null(mean_vals_df)) {
+        mean_vals_df = df
+    } else {
+        mean_vals_df = rbind(mean_vals_df, df)
+    }
+    
+}
+
+## fit linear model
+num_cells = ncells_partitions
+
+write.table(data.frame(num_cells=num_cells, sds=sds), file='num_cells_vs_sds.table.dat', quote=F, sep="\t")
+
+
+fit = lm(log(sds) ~ log(num_cells)) #note, hbadger does something similar, but not for the hmm cnv state levels
+
+my.spline = smooth.spline(log(num_cells), log(sds)) 
+
+message("plotting log(sd) vs. log(num_cells)")
+
+plot(log(num_cells), log(sds), main='log(sd) vs. log(num_cells)')
+
+plot(num_cells, sds, main='sd vs. num_cells')
+
+my.spline2 = smooth.spline(num_cells, sds) 
+
+## store mean_delta for the single gene for convenience sake
+mean_delta = qnorm(p=1-z_p_val, sd=sigma, mean=0)
+
+normal_sd_trend = list(mu=mu,
+                       sigma=sigma,
+                       fit=fit,
+                       spline=my.spline,
+                       mean_delta=mean_delta)
+
+
+
+### do some plotting
+
+
+for (ncells in ncells_partitions) {
+
+    message(sprintf("plotting ncells distribution: %g", ncells))
+    
+    data.want = mean_vals_df %>% filter(num_cells == ncells)
+    
+    
+    p = data.want %>% ggplot(aes(vals, fill=num_cells)) +
+        geom_density(alpha=0.3)
+
+    sigma <- exp(predict(normal_sd_trend$fit,
+                         newdata=data.frame(num_cells=ncells))[[1]]) 
+    
+    p = p +
+        stat_function(fun=dnorm, color='black', args=list('mean'=1,'sd'=sigma))  +
+        ggtitle(sprintf("num_cells: %g, sd: %g", ncells, sigma))
+
+    p = p +
+        stat_function(fun=dnorm, color='magenta', args=list('mean'=1,'sd'=num_cells_to_empirical_sd[[ ncells]] )) 
+    
+
+    if (FALSE) {
+    
+        spline.sd = exp(predict(my.spline, x=log(ncells))$y)
+        
+        
+        p = p +
+            stat_function(fun=dnorm, color='green', args=list('mean'=1,'sd'=spline.sd)) 
+        
+        spline2.sd = predict(my.spline2, x=ncells)$y
+        
+        message(spline2.sd)
+        
+        p = p +
+            stat_function(fun=dnorm, color='orange', args=list('mean'=1,'sd'=spline2.sd)) 
+    }
+    
+    plot(p)
+}
+
