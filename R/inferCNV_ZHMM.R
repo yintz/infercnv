f6d695f8345c7ae9695cd6f76554bd9e96bbfbb1
Brian Haas <brianjohnhaas@gmail.com>
2019-03-01 07:43:44 -0500

    setting KS cell count to 2
    Former-commit-id: f81475ce7f5ada0ab7ec243795ff8162ad455ae8
    Former-commit-id: c5652daf5b991be11651d73514c293ea8e422558

--- R/inferCNV_ZHMM.R
+++ R/inferCNV_ZHMM.R
@@ -1,12 +1,10 @@
 
-
 ## Based on number of cells in a clade
 .ZHMM_get_normal_sd_trend_by_num_cells_fit <- function(infercnv_obj, z_p_val=0.05, plot=TRUE) {
     
     if (!has_reference_cells(infercnv_obj)) {
         stop("Error, cannot tune parameters without reference 'normal' cells defined")
     }
-
 
     normal_samples = infercnv_obj@reference_grouped_cell_indices
     
@@ -19,7 +17,8 @@
     ngenes = nrow(normal_expr_vals)
 
     num_normal_samples = length(normal_samples)
-    message("-got ", num_normal_samples, " samples") 
+    flog.info(".ZHMM_get_normal_sd_trend_by_num_cells_fit:: -got ", num_normal_samples, " samples") 
+    
     for (ncells in seq_len(100)) {
         means = c()
         
@@ -46,22 +45,26 @@
         plot(log(num_cells), log(sds), main='log(sd) vs. log(num_cells)')
     }
     
-    ## store mean_delta for the single gene for convenience sake
+    ## get distribution position according to p_val in a qnorm
     mean_delta = determine_mean_delta_via_Z(sigma, p=z_p_val)    
 
+    ## do this HBadger style in case that option is to be used.
+    KS_delta = get_HoneyBADGER_setGexpDev(gexp.sd=sigma, alpha=z_p_val)
+    
     message("mean_delta: ", mean_delta, ", at sigma: ", sigma, ", and pval: ", z_p_val)
     
     normal_sd_trend = list(mu=mu,
                            sigma=sigma,
                            fit=fit,
-                           mean_delta=mean_delta)
+                           mean_delta=mean_delta,
+                           KS_delta=KS_delta)
     
     return(normal_sd_trend)
     
 }
 
 
-.ZHMM_get_HMM <- function(normal_sd_trend, num_cells, t, z_p_val=0.05) {
+.ZHMM_get_HMM <- function(normal_sd_trend, num_cells, t, z_p_val=0.05, use_KS=FALSE) {
 
     ## Here we do something very similar to HoneyBadger
     ## which is to estimate the mean/var for the CNV states
@@ -77,23 +80,20 @@
                                nrow=3)
     
     delta=c(t,     1-5*t,    t) # more likely normal,
-        
+    
     mu = normal_sd_trend$mu # normal cell mean
+    
+    mean_delta = ifelse(use_KS, normal_sd_trend$KS_delta, normal_sd_trend$mean_delta)
+    #flog.info(sprintf("-.ZHMM_get_HMM, mean_delta=%g, use_KS=%s", mean_delta, use_KS))
     
     if (num_cells == 1) {
         sigma = normal_sd_trend$sigma
-        mean_delta = normal_sd_trend$mean_delta
+        
     } else {
         ## use the var vs. num cells trend
         sigma <- exp(predict(normal_sd_trend$fit,
                              newdata=data.frame(num_cells=num_cells))[[1]])  
-
-        ## use t-test to determine adjusted mean value
         
-     
-        #mean_delta <- determine_mean_delta_via_Z(sigma, p=z_p_val)
-
-        mean_delta = normal_sd_trend$mean_delta
     }
     
     
@@ -118,7 +118,8 @@
 ZHMM_predict_CNV_via_HMM_on_indiv_cells  <- function(infercnv_obj,
                                                      z_p_val=0.05,
                                                      normal_sd_trend=.ZHMM_get_normal_sd_trend_by_num_cells_fit(infercnv_obj, z_p_val),
-                                                     t=1e-6) {
+                                                     t=1e-6,
+                                                     use_KS=FALSE) {
     
     flog.info("predict_CNV_via_HMM_on_indiv_cells()")
     
@@ -129,7 +130,7 @@ ZHMM_predict_CNV_via_HMM_on_indiv_cells  <- function(infercnv_obj,
     hmm.data = expr.data
     hmm.data[,] = -1 #init to invalid state
 
-    HMM_info  <- .ZHMM_get_HMM(normal_sd_trend, num_cells = 1, t=t, z_p_val=z_p_val)
+    HMM_info  <- .ZHMM_get_HMM(normal_sd_trend, num_cells = 1, t=t, z_p_val=z_p_val, use_KS=use_KS)
 
     message(HMM_info)
     
@@ -164,18 +165,17 @@ ZHMM_predict_CNV_via_HMM_on_indiv_cells  <- function(infercnv_obj,
 ZHMM_predict_CNV_via_HMM_on_tumor_subclusters  <- function(infercnv_obj,
                                                            z_p_val=0.05,
                                                            normal_sd_trend=.ZHMM_get_normal_sd_trend_by_num_cells_fit(infercnv_obj, z_p_val),
-                                                           t=1e-6
+                                                           t=1e-6,
+                                                           use_KS=FALSE
                                                            ) {
     
     
-    flog.info(sprintf("ZHMM_predict_CNV_via_HMM_on_tumor_subclusters(z_p_val=%g)", z_p_val))
-
+    flog.info(sprintf("ZHMM_predict_CNV_via_HMM_on_tumor_subclusters(z_p_val=%g, use_KS=%s)", z_p_val, use_KS))
+    
     if (is.null(infercnv_obj@tumor_subclusters)) {
         flog.warn("No subclusters defined, so instead running on whole samples")
-        return(ZHMM_predict_CNV_via_HMM_on_whole_tumor_samples(infercnv_obj, z_p_val, normal_sd_trend, t));
+        return(ZHMM_predict_CNV_via_HMM_on_whole_tumor_samples(infercnv_obj, z_p_val, normal_sd_trend, t, use_KS));
     }
-    
-    
     
     chrs = unique(infercnv_obj@gene_order$chr)
     
@@ -200,9 +200,8 @@ ZHMM_predict_CNV_via_HMM_on_tumor_subclusters  <- function(infercnv_obj,
             
             num_cells = length(tumor_subcluster_cells_idx)
 
-            HMM_info  <- .ZHMM_get_HMM(normal_sd_trend, num_cells=num_cells, t=t, z_p_val=z_p_val)
-                        
-                                    
+            HMM_info  <- .ZHMM_get_HMM(normal_sd_trend, num_cells=num_cells, t=t, z_p_val=z_p_val, use_KS=use_KS)
+                                                
             hmm <- HiddenMarkov::dthmm(gene_expr_vals,
                                        HMM_info[['state_transitions']],
                                        HMM_info[['delta']],
@@ -229,7 +228,8 @@ ZHMM_predict_CNV_via_HMM_on_tumor_subclusters  <- function(infercnv_obj,
 ZHMM_predict_CNV_via_HMM_on_whole_tumor_samples  <- function(infercnv_obj,
                                                         z_p_val=0.05,
                                                         normal_sd_trend=.ZHMM_get_normal_sd_trend_by_num_cells_fit(infercnv_obj, z_p_val),
-                                                        t=1e-6
+                                                        t=1e-6,
+                                                        use_KS=FALSE
                                                         ) {
     
     
@@ -257,7 +257,7 @@ ZHMM_predict_CNV_via_HMM_on_whole_tumor_samples  <- function(infercnv_obj,
             
             num_cells = length(tumor_sample_cells_idx)
             
-            HMM_info  <- .ZHMM_get_HMM(normal_sd_trend, num_cells=num_cells, t=t, z_p_val=z_p_val)
+            HMM_info  <- .ZHMM_get_HMM(normal_sd_trend, num_cells=num_cells, t=t, z_p_val=z_p_val, use_KS=use_KS)
             
             hmm <- HiddenMarkov::dthmm(gene_expr_vals,
                                        HMM_info[['state_transitions']],
@@ -306,3 +306,31 @@ determine_mean_delta_via_Z <- function(sigma, p) {
     
 }
 
+
+
+## This method is modified from HoneyBADGER's setGexpDev method
+## Essentially, using the KS test to determine where to set the
+## amp/del means for the distributions.
+
+get_HoneyBADGER_setGexpDev <- function(gexp.sd, alpha, n=100, seed=0, plot=FALSE) {
+    k = 2 # set to 101 in HB.  Here we set it to the min num of cells needed for a ks test.
+    set.seed(seed)
+
+    devs <- seq(0, gexp.sd, gexp.sd/10)
+    pvs <- unlist(lapply(devs, function(dev) {
+        mean(unlist(lapply(seq_len(n), function(i) {
+            pv <- ks.test(rnorm(k, 0, gexp.sd), rnorm(k, dev, gexp.sd))
+            pv$p.value
+        })))
+    }))
+    if(plot) {
+        plot(pvs, devs, xlab="p-value", ylab="deviation", xlim=c(0,1))
+    }
+    fit <- lm(devs ~ pvs)
+
+    optim.dev <- predict(fit, newdata=data.frame(pvs=alpha))
+
+    flog.info(sprintf("-get_HoneyBADGER_setGexpDev(sigma=%g, alpha=%g) = %g", gexp.sd, alpha, optim.dev))
+    
+    return(optim.dev)
+}
