966904d13beb8c76bf06a8127a6e990751996de6
GeorgescuC <GeorgescuC@users.noreply.github.com>
2019-04-08 15:36:03 -0400

    Added script that takes as input a regular counts matrix, converts it to a sparseMatrix and saves it as an RDS file for loading in the main process.
    Former-commit-id: 61a6849bd8ff9eee1efe6a99da44080f028f6df4 [formerly 61a6849bd8ff9eee1efe6a99da44080f028f6df4 [formerly cf74109337c15d72f48b726b86662ea118629e16]]
    Former-commit-id: ef524b25e281bdaf1e80ac2fe2e6455944165ce0
    Former-commit-id: d5b4191d40c027edec77cb1964fbc44becaceb47
    Former-commit-id: 65e7c2cb70e3ae8e499dc9b560a238c147fedac0

---
+++ scripts/prepare_sparsematrix.R
@@ -0,0 +1,88 @@
+#!/usr/bin/env Rscript
+
+
+library(optparse)
+library(Matrix)
+library(data.table)
+library(logging
+
+
+
+
+logging::basicConfig(level='INFO')
+
+pargs <- optparse::OptionParser(usage=paste("%prog [options]",
+                                            "--input data_matrix ",
+                                            "--output sparse_matrix ",
+                                            "--delim matrix_delimiter"
+                                            ))
+
+pargs <- optparse::add_option(pargs, c("--input"),
+                              type="character",
+                              default=NULL,
+                              action="store",
+                              dest="input",
+                              metavar="input",
+                              help=paste("Input raw counts matrix ",
+                              			 "to prepare for infercnv run."))
+
+pargs <- optparse::add_option(pargs, c("--output"),
+                              type="character",
+                              default=NULL,
+                              action="store",
+                              dest="output",
+                              metavar="output",
+                              help=paste("Output raw counts matrix ",
+                              			 "as a sparseMatrix for infercnv run."))
+
+pargs <- optparse::add_option(pargs, c("--delim"),
+                              type="character",
+                              action="store",
+                              default="\t",
+                              dest="delim",
+                              metavar="delim",
+                              help=paste("Delimiter for reading expression matrix",
+                                         "[Default %default]"))
+
+args <- optparse::parse_args(pargs)
+
+if (is.null(args$input) || is.null(args$output)) {
+	logging::logerror("Please provide input and output arguments")
+}
+
+data_head = fread(input=args$input,
+	  sep=args$delim,
+	  header=FALSE,
+	  nrows=1,
+	  stringsAsFactors=FALSE,
+	  check.names=FALSE,
+	  nThread=1,
+	  logical01=FALSE,
+	  data.table=FALSE)
+
+ddata = fread(input=args$input,
+	  sep=args$delim,
+	  header=FALSE,
+	  skip=1,
+	  stringsAsFactors=FALSE,
+	  check.names=FALSE,
+	  nThread=1,
+	  logical01=FALSE,
+	  data.table=FALSE)
+
+
+# store column names before dropping the column from the matrix
+saved_names = as.vector(unlist(ddata[, 1]))
+ddata = ddata[, -1, drop=FALSE]
+
+
+colnames(ddata) = as.vector(unlist(data_head))
+basic_matrix = as.matrix(ddata)
+rm(ddata)  # make memory available
+sparse_matrix = Matrix(basic_matrix, sparse=T)
+rm(basic_matrix)  # make memory available
+row.names(sparse_matrix) = saved_names
+
+saveRDS(sparse_matrix, file=paste(args$output, "rds", sep="."))
+
+
