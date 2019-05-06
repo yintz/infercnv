9382627600c3024483f14686a8e5205c23ee7ded
M. Brown <35315693+brownmp@users.noreply.github.com>
2019-05-06 16:03:00 -0400

    Added ability to reassign CNV states, plot before and after normal probabilities, no_plot prevents plotting Bayes network probabilities.
    Add option to reassign HMM identified CNV states based on probability of each state. Plot the normal probabilities before and after filtering. The flag "no_plot" now prevents plotting Bayes network probabilities.

--- R/inferCNV_ops.R
+++ R/inferCNV_ops.R
@@ -231,6 +231,7 @@ run <- function(infercnv_obj,
                 #sim_method=c('meanvar', 'simple', 'splatter'), ## only meanvar supported, others experimental
                 sim_method='meanvar',
                 sim_foreground=FALSE, ## experimental
+                reassignCNVs=TRUE,
 
 
                 ## tumor subclustering options
@@ -1017,6 +1018,7 @@ run <- function(infercnv_obj,
                 mcmc_obj <- infercnv::inferCNVBayesNet( infercnv_obj     = infercnv_obj_prelim,
                                                        HMM_states        = hmm.infercnv_obj@expr.data,
                                                        file_dir          = out_dir,
+                                                       no_plot           = no_plot,
                                                        postMcmcMethod    = "removeCNV",
                                                        out_dir           = file.path(out_dir, sprintf("BayesNetOutput.%s", hmm_resume_file_token)),
                                                        quietly           = TRUE,
@@ -1025,7 +1027,8 @@ run <- function(infercnv_obj,
                                                        diagnostics       = diagnostics,
                                                        HMM_type          = HMM_type,
                                                        k_obs_groups      = k_obs_groups,
-                                                       cluster_by_groups = cluster_by_groups)
+                                                       cluster_by_groups = cluster_by_groups,
+                                                       reassignCNVs      = reassignCNVs)
                 saveRDS(mcmc_obj, file=mcmc_obj_file)
             }
             
