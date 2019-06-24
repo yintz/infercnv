76c537cec23b12c0a81f70b3f192952190a72cdf
GeorgescuC <GeorgescuC@users.noreply.github.com>
2019-06-24 17:20:41 -0400

    Fix not keeping new options after reloading up to a given step.

--- R/inferCNV_ops.R
+++ R/inferCNV_ops.R
@@ -337,6 +337,7 @@ run <- function(infercnv_obj,
                         invisible(gc())
                     }
                     else {
+                        hmm.infercnv_obj@options = infercnv_obj@options
                         skip_hmm = i - 16
                         # if (i == 19) {
                         #     skip_hmm = 2
@@ -360,8 +361,10 @@ run <- function(infercnv_obj,
                     reloaded_infercnv_obj = readRDS(reload_info$expected_file_names[[i]])
 
                     if (.compare_args(infercnv_obj@options, unlist(reload_info$relevant_args[1:i]), reloaded_infercnv_obj@options)) {
+                        options_backup = infercnv_obj@options
                         infercnv_obj = reloaded_infercnv_obj # replace input infercnv_obj
                         rm(reloaded_infercnv_obj) # remove first (temporary) reference so there's no duplication when they would diverge
+                        infercnv_obj@options = options_backup
                         skip_past = i
                         break
                     }
