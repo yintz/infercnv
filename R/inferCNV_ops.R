ce40e4e54c5f7212ac1d5025b1fcc56afdda5f11
GeorgescuC <GeorgescuC@users.noreply.github.com>
2021-06-23 23:23:08 -0400

    Fix name generation error for step 15. Reorganized the initialization and argument passing for the save/reload to remove redundancy in default variable values and instead pull them from the same source.

--- R/inferCNV_ops.R
+++ R/inferCNV_ops.R
@@ -346,12 +346,37 @@ run <- function(infercnv_obj,
     arg_names = names(call_match)
     arg_names = arg_names[-which(arg_names == "" | arg_names == "infercnv_obj")]
 
+    # evaluate variables such as output_dir
     for (n in arg_names) {
         call_match[[n]] = get(n)
     }
 
-    
+    # add the arguments that are checked against a list of allowed values and autofilled with the first if none is given using match.arg()
+    call_match$smooth_method = smooth_method
+    call_match$HMM_type = HMM_type
+    call_match$HMM_report_by = HMM_report_by
+    call_match$analysis_mode = analysis_mode
+    call_match$tumor_subcluster_partition_method = tumor_subcluster_partition_method
+    # add argument needed to get relevant args list
+    call_match$max_centered_threshold = max_centered_threshold
+    call_match$remove_genes_at_chr_ends = remove_genes_at_chr_ends
+    call_match$prune_outliers = prune_outliers
+    call_match$BayesMaxPNormal = BayesMaxPNormal
+    call_match$mask_nonDE_genes = mask_nonDE_genes
+    call_match$noise_filter = noise_filter
+    call_match$sd_amplifier = sd_amplifier
+    call_match$noise_logistic = noise_logistic
+
+    call_match$infercnv_obj = NULL
+
+    # pull the list of argument that are set by the user in case they differ from the default
     current_args = as.list(call_match[-which(names(call_match) == "" | names(call_match) == "infercnv_obj")])
+    # add the arguments that are checked against a list of allowed values and autofilled with the first if none is given using match.arg()
+    # current_args$smooth_method = smooth_method
+    # current_args$HMM_type = HMM_type
+    # current_args$HMM_report_by = HMM_report_by
+    # current_args$analysis_mode = analysis_mode
+    # current_args$tumor_subcluster_partition_method = tumor_subcluster_partition_method
 
     if(out_dir != "." && !file.exists(out_dir)){
         flog.info(paste0("Creating output path ", out_dir))
@@ -3081,23 +3106,26 @@ compareNA <- function(v1,v2) {
 #' @noRd
 #'
 
-.get_relevant_args_list <- function(out_dir=NULL,
-                                    HMM=FALSE,
-                                    HMM_type='i6',
-                                    num_ref_groups=NULL,
-                                    analysis_mode='samples',
-                                    tumor_subcluster_partition_method='random_trees',
-                                    smooth_method='pyramidinal',
-                                    max_centered_threshold=3,
-                                    remove_genes_at_chr_ends=FALSE,
-                                    prune_outliers=FALSE,
-                                    BayesMaxPNormal=0.5,
-                                    mask_nonDE_genes=FALSE,
-                                    denoise=FALSE,
-                                    noise_filter=NA,
-                                    sd_amplifier=1.5,
-                                    noise_logistic=FALSE,
+.get_relevant_args_list <- function(
+# .get_relevant_args_list <- function(out_dir=NULL,
+#                                     HMM=FALSE,
+#                                     HMM_type='i6',
+#                                     num_ref_groups=NULL,
+#                                     analysis_mode='samples',
+#                                     tumor_subcluster_partition_method,
+#                                     smooth_method='pyramidinal',
+#                                     max_centered_threshold=3,
+#                                     remove_genes_at_chr_ends=FALSE,
+#                                     prune_outliers=FALSE,
+#                                     BayesMaxPNormal=0.5,
+#                                     mask_nonDE_genes=FALSE,
+#                                     denoise=FALSE,
+#                                     noise_filter=NA,
+#                                     sd_amplifier=1.5,
+#                                     noise_logistic=FALSE,
                                     ...) {
+    run_arguments = list(...)
+
     # creation args  ## add check for matrix size, col/row names, and ref/obs indices
     relevant_args = vector("list", 21) # 21 = max steps
     expected_file_names = vector("character", 21)
@@ -3115,13 +3143,13 @@ compareNA <- function(v1,v2) {
 
     # 3 _normalized_by_depth%s.infercnv_obj
     relevant_args[[step_i]] = c("HMM")
-    if (HMM) {
+    if (run_arguments$HMM) {
         relevant_args[[step_i]] = c(relevant_args[[step_i]], "HMM_type") 
-        if (HMM_type == "i6") {
+        if (run_arguments$HMM_type == "i6") {
             relevant_args[[step_i]] = c(relevant_args[[step_i]], "sim_method", "hspike_aggregate_normals", "sim_foreground")
         }
     }
-    resume_file_token = ifelse((HMM), paste0("HMM", HMM_type), "")
+    resume_file_token = ifelse((run_arguments$HMM), paste0("HMM", run_arguments$HMM_type), "")
     expected_file_names[[step_i]] = file.path(out_dir, sprintf("%02d_normalized_by_depth%s.infercnv_obj", step_i, resume_file_token))
     step_i = step_i + 1
 
@@ -3137,18 +3165,18 @@ compareNA <- function(v1,v2) {
 
     # 6 _split_%sf_refs%s.infercnv_obj
     relevant_args[[step_i]] = c("num_ref_groups")
-    if (!is.null(num_ref_groups)) {
+    if (!is.null(run_arguments$num_ref_groups)) {
         relevant_args[[step_i]] = c(relevant_args[[step_i]], "hclust_method")
-        expected_file_names[[step_i]] = file.path(out_dir, sprintf("%02d_split_%sf_refs%s.infercnv_obj", step_i, resume_file_token, num_ref_groups))
+        expected_file_names[[step_i]] = file.path(out_dir, sprintf("%02d_split_%sf_refs%s.infercnv_obj", step_i, resume_file_token, run_arguments$num_ref_groups))
     }
     step_i = step_i + 1
 
     # 7 _tumor_subclusters%s.%s.infercnv_obj
     relevant_args[[step_i]] = c("analysis_mode", "tumor_subcluster_partition_method")
-    if (analysis_mode == 'subclusters' & tumor_subcluster_partition_method == 'random_trees') {
+    if (run_arguments$analysis_mode == 'subclusters' & run_arguments$tumor_subcluster_partition_method == 'random_trees') {
         resume_file_token = paste0(resume_file_token, ".rand_trees")
         relevant_args[[step_i]] = c(relevant_args[[step_i]], "hclust_method", "tumor_subcluster_pval", "cluster_by_groups")
-        expected_file_names[[step_i]] = file.path(out_dir, sprintf("%02d_tumor_subclusters%s.%s.infercnv_obj", step_i, resume_file_token, tumor_subcluster_partition_method))
+        expected_file_names[[step_i]] = file.path(out_dir, sprintf("%02d_tumor_subclusters%s.%s.infercnv_obj", step_i, resume_file_token, run_arguments$tumor_subcluster_partition_method))
     }
     step_i = step_i + 1
 
@@ -3159,7 +3187,7 @@ compareNA <- function(v1,v2) {
 
     # 9 _apply_max_centered_expr_threshold%s.infercnv_obj
     relevant_args[[step_i]] = c("max_centered_threshold")
-    if(!is.na(max_centered_threshold)) {
+    if(!is.na(run_arguments$max_centered_threshold)) {
         relevant_args[[step_i]] = c(relevant_args[[step_i]], "max_centered_threshold")
         expected_file_names[[step_i]] = file.path(out_dir, sprintf("%02d_apply_max_centered_expr_threshold%s.infercnv_obj", step_i, resume_file_token))
     }
@@ -3167,7 +3195,7 @@ compareNA <- function(v1,v2) {
 
     # 10 _smoothed_by_chr%s.infercnv_obj
     relevant_args[[step_i]] = c("smooth_method", "window_length")
-    if (smooth_method == 'pyramidinal') {
+    if (run_arguments$smooth_method == 'pyramidinal') {
         relevant_args[[step_i]] = c(relevant_args[[step_i]], "smooth_ends")
     }
     expected_file_names[[step_i]] = file.path(out_dir, sprintf("%02d_smoothed_by_chr%s.infercnv_obj", step_i, resume_file_token))
@@ -3185,7 +3213,7 @@ compareNA <- function(v1,v2) {
 
     # 13 _remove_gene_at_chr_ends%s.infercnv_obj
     relevant_args[[step_i]] = c("remove_genes_at_chr_ends")
-    if (remove_genes_at_chr_ends == TRUE) {
+    if (run_arguments$remove_genes_at_chr_ends == TRUE) {
         relevant_args[[step_i]] = c(relevant_args[[step_i]], "window_length")
         expected_file_names[[step_i]] = file.path(out_dir, sprintf("%02d_remove_gene_at_chr_ends%s.infercnv_obj", step_i, resume_file_token))
     }
@@ -3198,12 +3226,12 @@ compareNA <- function(v1,v2) {
 
     # 15 _tumor_subclusters%s.infercnv_obj
     relevant_args[[step_i]] = c("analysis_mode", "tumor_subcluster_partition_method")
-    if (analysis_mode == 'subclusters' & tumor_subcluster_partition_method != 'random_trees') {
-        resume_file_token = paste0(resume_file_token, '.', tumor_subcluster_partition_method)
+    if (run_arguments$analysis_mode == 'subclusters' & run_arguments$tumor_subcluster_partition_method != 'random_trees') {
+        resume_file_token = paste0(resume_file_token, '.', run_arguments$tumor_subcluster_partition_method)
         relevant_args[[step_i]] = c(relevant_args[[step_i]], "k_nn", "leiden_resolution", "tumor_subcluster_pval", "hclust_method", "cluster_by_groups")
         expected_file_names[[step_i]] = file.path(out_dir, sprintf("%02d_tumor_subclusters%s.infercnv_obj", step_i, resume_file_token))
     }
-    else if (analysis_mode != 'subclusters') {
+    else if (run_arguments$analysis_mode != 'subclusters') {
         relevant_args[[step_i]] = c(relevant_args[[step_i]], "tumor_subcluster_pval", "hclust_method", "cluster_by_groups")
         expected_file_names[[step_i]] = file.path(out_dir, sprintf("%02d_no_subclustering%s.infercnv_obj", step_i, resume_file_token))
     }
@@ -3211,7 +3239,7 @@ compareNA <- function(v1,v2) {
 
     # 16 _remove_outlier%s.infercnv_obj
     relevant_args[[step_i]] = c("prune_outliers")
-    if (prune_outliers) {
+    if (run_arguments$prune_outliers) {
         relevant_args[[step_i]] = c(relevant_args[[step_i]], "outlier_method_bound", "outlier_lower_bound", "outlier_upper_bound")
         expected_file_names[[step_i]] = file.path(out_dir, sprintf("%02d_remove_outlier%s.infercnv_obj", step_i, resume_file_token))
     }
@@ -3219,15 +3247,15 @@ compareNA <- function(v1,v2) {
 
     # 17 _HMM_pred%s.infercnv_obj
     relevant_args[[step_i]] = c("HMM")
-    if (HMM) {
+    if (run_arguments$HMM) {
         relevant_args[[step_i]] = c(relevant_args[[step_i]], "HMM_type", "analysis_mode", "HMM_transition_prob", "HMM_report_by")
-        hmm_resume_file_token = paste0(resume_file_token, ".hmm_mode-", analysis_mode)
+        hmm_resume_file_token = paste0(resume_file_token, ".hmm_mode-", run_arguments$analysis_mode)
         # hmm.infercnv_obj_file = file.path(out_dir, sprintf("%02d_HMM_pred%s.infercnv_obj", step_i, hmm_resume_file_token))  ########
-        if (analysis_mode == 'subclusters' && tumor_subcluster_partition_method == 'random_trees') {
+        if (run_arguments$analysis_mode == 'subclusters' && run_arguments$tumor_subcluster_partition_method == 'random_trees') {
             relevant_args[[step_i]] = c(relevant_args[[step_i]], "hclust_method")
         }
 
-        if (HMM_type == "i6") {
+        if (run_arguments$HMM_type == "i6") {
             # relevant_args[[step_i]] = c(relevant_args[[step_i]], )
 
         }
@@ -3240,7 +3268,7 @@ compareNA <- function(v1,v2) {
 
     # 18 _HMM_pred.Bayes_Net%s.mcmc_obj
     relevant_args[[step_i]] = c("HMM", "BayesMaxPNormal")
-    if (HMM == TRUE & BayesMaxPNormal > 0) {
+    if (run_arguments$HMM == TRUE & run_arguments$BayesMaxPNormal > 0) {
         relevant_args[[step_i]] = c(relevant_args[[step_i]], "diagnostics", "reassignCNVs")
         # mcmc_obj_file = file.path(out_dir, sprintf("%02d_HMM_pred.Bayes_Net%s.mcmc_obj", step_i, hmm_resume_file_token))
         # mcmc.infercnv_obj_file = file.path(out_dir, sprintf("%02d_HMM_pred.Bayes_Net%s.Pnorm_%g.infercnv_obj", step_i, hmm_resume_file_token, BayesMaxPNormal))
@@ -3251,27 +3279,27 @@ compareNA <- function(v1,v2) {
 
     # 19 _HMM_pred.Bayes_Net%s.Pnorm_%g.infercnv_obj
     relevant_args[[step_i]] = c("HMM", "BayesMaxPNormal")
-    if (HMM == TRUE & BayesMaxPNormal > 0) {
+    if (run_arguments$HMM == TRUE & run_arguments$BayesMaxPNormal > 0) {
         # relevant_args[[step_i]] = c(relevant_args[[step_i]], "diagnostics", "reassignCNVs")
         # mcmc_obj_file = file.path(out_dir, sprintf("%02d_HMM_pred.Bayes_Net%s.mcmc_obj", step_i, hmm_resume_file_token))
         # mcmc.infercnv_obj_file = file.path(out_dir, sprintf("%02d_HMM_pred.Bayes_Net%s.Pnorm_%g.infercnv_obj", step_i, hmm_resume_file_token, BayesMaxPNormal))
         # expected_file_names[[step_i]] = file.path(out_dir, sprintf("%02d_HMM_pred.Bayes_Net%s.mcmc_obj", step_i, hmm_resume_file_token)), 
-        expected_file_names[[step_i]] = file.path(out_dir, sprintf("%02d_HMM_pred.Bayes_Net%s.Pnorm_%g.infercnv_obj", step_i, hmm_resume_file_token, BayesMaxPNormal))
+        expected_file_names[[step_i]] = file.path(out_dir, sprintf("%02d_HMM_pred.Bayes_Net%s.Pnorm_%g.infercnv_obj", step_i, hmm_resume_file_token, run_arguments$BayesMaxPNormal))
     }
     step_i = step_i + 1
 
     # 20 _HMM_pred.repr_intensities%s.Pnorm_%g.infercnv_obj
     relevant_args[[step_i]] = c("HMM")
-    if (HMM) {
+    if (run_arguments$HMM) {
         relevant_args[[step_i]] = c(relevant_args[[step_i]], "BayesMaxPNormal")
         # hmm.infercnv_obj_file = file.path(out_dir, sprintf("%02d_HMM_pred.repr_intensities%s.Pnorm_%g.infercnv_obj", step_i, hmm_resume_file_token, BayesMaxPNormal))
-        expected_file_names[[step_i]] = file.path(out_dir, sprintf("%02d_HMM_pred.repr_intensities%s.Pnorm_%g.infercnv_obj", step_i, hmm_resume_file_token, BayesMaxPNormal))
+        expected_file_names[[step_i]] = file.path(out_dir, sprintf("%02d_HMM_pred.repr_intensities%s.Pnorm_%g.infercnv_obj", step_i, hmm_resume_file_token, run_arguments$BayesMaxPNormal))
     }
     step_i = step_i + 1
 
     # 21 _mask_nonDE%s.infercnv_obj
     relevant_args[[step_i]] = c("mask_nonDE_genes")
-    if (mask_nonDE_genes) {
+    if (run_arguments$mask_nonDE_genes) {
         relevant_args[[step_i]] = c(relevant_args[[step_i]], "require_DE_all_normals", "test.use", "mask_nonDE_pval")
         expected_file_names[[step_i]] = file.path(out_dir, sprintf("%02d_mask_nonDE%s.infercnv_obj", step_i, resume_file_token))
     }
@@ -3279,9 +3307,9 @@ compareNA <- function(v1,v2) {
 
     # 22 _denoise%s.NF_%s.SD_%g.NL_%s.infercnv_obj
     relevant_args[[step_i]] = c("denoise")
-    if (denoise) {
+    if (run_arguments$denoise) {
         relevant_args[[step_i]] = c(relevant_args[[step_i]], "noise_filter", "sd_amplifier", "noise_logistic")
-        expected_file_names[[step_i]] = file.path(out_dir, sprintf("%02d_denoise%s.NF_%s.SD_%g.NL_%s.infercnv_obj", step_i, resume_file_token, noise_filter, sd_amplifier, noise_logistic))
+        expected_file_names[[step_i]] = file.path(out_dir, sprintf("%02d_denoise%s.NF_%s.SD_%g.NL_%s.infercnv_obj", step_i, resume_file_token, run_arguments$noise_filter, run_arguments$sd_amplifier, run_arguments$noise_logistic))
     }
     step_i = step_i + 1
 
